b952c980649f90fde8e7722e292c21524efb0a7e
JiriOndrusek <ondrusek.jiri@gmail.com>
2020-07-10 16:27:39 +0200

    first try with upgraded component

--- extensions/cassandraql/runtime/src/main/java/org/apache/camel/quarkus/component/cassandraql/CassandraRecorder.java
+++ extensions/cassandraql/runtime/src/main/java/org/apache/camel/quarkus/component/cassandraql/CassandraRecorder.java
@@ -16,36 +16,16 @@
  */
 package org.apache.camel.quarkus.component.cassandraql;
 
+import java.util.Map;
+
 import com.datastax.oss.quarkus.runtime.api.session.QuarkusCqlSession;
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
-import io.quarkus.tika.TikaContent;
-import io.quarkus.tika.TikaMetadata;
-import io.quarkus.tika.TikaParser;
-import io.quarkus.tika.runtime.TikaParserProducer;
 import org.apache.camel.Component;
 import org.apache.camel.Endpoint;
-import org.apache.camel.Producer;
 import org.apache.camel.component.cassandra.CassandraComponent;
 import org.apache.camel.component.cassandra.CassandraEndpoint;
-import org.apache.camel.component.tika.TikaComponent;
-import org.apache.camel.component.tika.TikaConfiguration;
-import org.apache.camel.component.tika.TikaEndpoint;
-import org.apache.camel.component.tika.TikaProducer;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 
 @Recorder
 public class CassandraRecorder {
