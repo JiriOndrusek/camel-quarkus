4987ebf8b474812be5f90d2a46b3535313e5d475
JiriOndrusek <ondrusek.jiri@gmail.com>
2020-08-27 12:00:52 +0200

    blocked on Java_com_sun_jna_Native_malloc__J]

--- extensions/djl/deployment/src/main/java/org/apache/camel/quarkus/component/djl/deployment/DjlProcessor.java
+++ extensions/djl/deployment/src/main/java/org/apache/camel/quarkus/component/djl/deployment/DjlProcessor.java
@@ -17,15 +17,21 @@
 package org.apache.camel.quarkus.component.djl.deployment;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-import ai.djl.repository.zoo.LocalZooProvider;
+import ai.djl.mxnet.zoo.MxModelZoo;
 import ai.djl.repository.zoo.ZooProvider;
 import com.google.gson.JsonSerializer;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
+import io.quarkus.deployment.util.ServiceUtil;
+import org.jboss.jandex.IndexView;
 
 class DjlProcessor {
 
@@ -44,28 +50,60 @@ class DjlProcessor {
 
     @BuildStep
     void registerNativeImageResources(BuildProducer<ServiceProviderBuildItem> services) throws IOException {
-        //        String service = "META-INF/services/" + ZooProvider.class.getName();
-        //
-        //        // find out all the implementation classes listed in the service files
-        //        Set<String> implementations = ServiceUtil.classNamesNamedIn(Thread.currentThread().getContextClassLoader(),
-        //                service);
-        //
-        //        System.out.println("--------------------------------");
-        //        System.out.println("registering for: " + ZooProvider.class.getName());
-        //        System.out.println(implementations);
-        //        System.out.println("--------------------------------");
-        // register every listed implementation class so they can be instantiated
-        // in native-image at run-time
-        //        services.produce(
-        //                new ServiceProviderBuildItem(ZooProvider.class.getName(),
-        //                        implementations.toArray(new String[0])));
+        String service = "META-INF/services/" + ZooProvider.class.getName();
 
-        System.out.println("--------------------------------");
-        System.out.println("registering for: " + ZooProvider.class.getName());
-        System.out.println(LocalZooProvider.class.getName());
-        System.out.println("--------------------------------");
+        // find out all the implementation classes listed in the service files
+        Set<String> implementations = ServiceUtil.classNamesNamedIn(Thread.currentThread().getContextClassLoader(),
+                service);
+
+        //         register every listed implementation class so they can be instantiated
+        //         in native-image at run-time
         services.produce(
-                new ServiceProviderBuildItem(ZooProvider.class.getName(), LocalZooProvider.class.getName()));
+                new ServiceProviderBuildItem(ZooProvider.class.getName(),
+                        implementations.toArray(new String[0])));
 
     }
+
+    @BuildStep
+    ReflectiveClassBuildItem registerModelProvidersForReflection() throws IOException {
+        String service = "META-INF/services/" + ZooProvider.class.getName();
+
+        // find out all the implementation classes listed in the service files
+        Set<String> implementations = ServiceUtil.classNamesNamedIn(Thread.currentThread().getContextClassLoader(),
+                service);
+        //todo jondruse do it properly, find all models
+        implementations = new HashSet<>(implementations);
+        implementations.add(MxModelZoo.class.getCanonicalName());
+
+        return new ReflectiveClassBuildItem(false, true, implementations.stream().map(name -> {
+            try {
+                return Class.forName(name);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }).toArray(Class[]::new));
+    }
+
+    @BuildStep
+    ReflectiveClassBuildItem registerForReflection(CombinedIndexBuildItem combinedIndex) {
+        IndexView index = combinedIndex.getIndex();
+        String[] dtos = index.getKnownClasses().stream()
+                .map(ci -> ci.name().toString())
+                .filter(n -> n.startsWith("ai.djl.repository"))
+                .sorted()
+                //todo jondruse testing purposes
+                .peek(System.out::println)
+                .toArray(String[]::new);
+
+        //todo jondruse, is it necessary to use true, true?
+        return new ReflectiveClassBuildItem(true, true, dtos);
+    }
+
+    @BuildStep
+    IndexDependencyBuildItem registerZDependencyForIndex() {
+        return new IndexDependencyBuildItem("ai.djl", "repository");
+    }
+
+    //todo jondruse add substitute to MXEngine.public boolean hasCapability(String capability)  it detectcs CUDA
+
 }
