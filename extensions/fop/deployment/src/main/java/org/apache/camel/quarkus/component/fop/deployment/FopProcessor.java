29ae649b6d81feed858cd40aa47bfb17ee76ab97
JiriOndrusek <ondrusek.jiri@gmail.com>
2020-09-14 13:53:15 +0200

    do not merge finalizing WIP 01

--- extensions/fop/deployment/src/main/java/org/apache/camel/quarkus/component/fop/deployment/FopProcessor.java
+++ extensions/fop/deployment/src/main/java/org/apache/camel/quarkus/component/fop/deployment/FopProcessor.java
@@ -16,6 +16,7 @@
  */
 package org.apache.camel.quarkus.component.fop.deployment;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +29,9 @@ import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
-import org.apache.fop.render.intermediate.IFUtil;
+import org.apache.fop.render.RendererEventProducer;
+import org.apache.fop.render.pdf.PDFDocumentHandlerMaker;
+import org.apache.fop.render.pdf.extensions.PDFExtensionHandlerFactory;
 import org.apache.xmlgraphics.image.loader.spi.ImageImplRegistry;
 import org.jboss.jandex.IndexView;
 
@@ -47,34 +50,17 @@ class FopProcessor {
 
         List<String> dtos = index.getKnownClasses().stream()
                 .map(ci -> ci.name().toString())
-                .filter(n -> n.endsWith("ElementMapping")/*|| /* todo jondruse testing n.contains("xalan")*/)
+                .filter(n -> n.endsWith("ElementMapping"))
                 .sorted()
-                .peek(System.out::println)
                 .collect(Collectors.toList());
 
-        dtos.add("org.apache.fop.render.pdf.extensions.PDFExtensionHandlerFactory");
-        dtos.add("org.apache.fop.render.pdf.PDFDocumentHandlerMaker");
-        dtos.add("org.apache.fop.render.RendererEventProducer");
-        dtos.add("org.apache.fop.events.model.EventProducerModel"); //needs methods
-        dtos.add("org.apache.fop.events.mode.SAXTransformerFactory");
-        dtos.add("java.io.IOException");
-        dtos.add("java.lang.Integer");
+        dtos.add(PDFExtensionHandlerFactory.class.getCanonicalName());
+        dtos.add(PDFDocumentHandlerMaker.class.getCanonicalName());
+        dtos.add(RendererEventProducer.class.getCanonicalName());
+        dtos.add(IOException.class.getCanonicalName());
+        dtos.add(Integer.class.getCanonicalName());
 
-        return new ReflectiveClassBuildItem(true, false, dtos.toArray(new String[dtos.size()]));
-        //        return new ReflectiveClassBuildItem(false, false, new String[] { "org.apache.fop.fo.ElementMapping",
-        //                "org.apache.fop.fo.FOElementMapping", "org.apache.fop.render.pdf.extensions.PDFElementMapping",
-        //                "org.apache.fop.fo.extensions.InternalElementMapping",
-        //                "org.apache.fop.fo.extensions.OldExtensionElementMapping" });
-    }
-
-    @BuildStep
-    IndexDependencyBuildItem registerDependencyForIndex() {
-        return new IndexDependencyBuildItem("org.jboss.logging", "commons-logging-jboss-logging");
-    }
-
-    @BuildStep
-    IndexDependencyBuildItem registerDependencyForInde2() {
-        return new IndexDependencyBuildItem("xalan", "xalan");
+        return new ReflectiveClassBuildItem(false, false, dtos.toArray(new String[dtos.size()]));
     }
 
     @BuildStep
@@ -85,60 +71,21 @@ class FopProcessor {
 
     @BuildStep
     NativeImageResourceBuildItem initResources() {
-        //use regex, once this is implemented https://github.com/quarkusio/quarkus/issues/7033
         return new NativeImageResourceBuildItem(
                 "META-INF/services/org.apache.fop.fo.ElementMapping",
                 "META-INF/services/org.apache.fop.render.intermediate.IFDocumentHandler",
-                //                "/org/apache/fop/render/event-model.xml",
-                //                "/event-model.xml",
-                "org/apache/fop/render/event-model.xml",
-                "com/sun/org/apache/xerces/internal/impl/msg/SAXMessages");
-        //                "event-model.xml")
+                "org/apache/fop/render/event-model.xml");
     }
 
     @BuildStep
-    NativeImageResourceBuildItem initResources2() {
-        //use regex, once this is implemented https://github.com/quarkusio/quarkus/issues/7033
-        return new NativeImageResourceBuildItem(
-                "META-INF/services/org.apache.fop.util.ContentHandlerFactory",
-                "org/apache/fop/render/event-model.xml",
-                "com.sun.org.apache.xerces.internal.impl.msg.SAXMessages");
-    }
-
-    @BuildStep
-    NativeImageResourceBundleBuildItem initResources3() {
+    NativeImageResourceBundleBuildItem initBundles() {
         return new NativeImageResourceBundleBuildItem(
                 "com.sun.org.apache.xerces.internal.impl.msg.SAXMessages");
     }
 
-    //    @BuildStep
-    //    void nativeImageResources(BuildProducer<NativeImageResourceBuildItem> nativeImage) {
-    //        nativeImage.produce(new NativeImageResourceBuildItem(
-    //                "META-INF/services/org.apache.fop.events.EventExceptionManager$ExceptionFactory",
-    //                "META-INF/services/org.apache.fop.render.Renderer",
-    //                "META-INF/services/org.apache.fop.util.text.AdvancedMessageFormat$PartFactory",
-    //                "META-INF/services/org.apache.fop.fo.ElementMapping",
-    //                "META-INF/services/org.apache.fop.render.XMLHandler",
-    //                "META-INF/services/org.apache.xmlgraphics.image.loader.spi.ImageConverter",
-    //                "META-INF/services/org.apache.fop.fo.FOEventHandler",
-    //                "META-INF/services/org.apache.fop.util.ContentHandlerFactory",
-    //                "META-INF/services/org.apache.xmlgraphics.image.loader.spi.ImageLoaderFactory",
-    //                "META-INF/services/org.apache.fop.render.ImageHandler",
-    //                "META-INF/services/org.apache.fop.util.text.AdvancedMessageFormat$Function",
-    //                "META-INF/services/org.apache.xmlgraphics.image.loader.spi.ImagePreloader",
-    //                "META-INF/services/org.apache.fop.render.intermediate.IFDocumentHandler",
-    //                "META-INF/services/org.apache.fop.util.text.AdvancedMessageFormat$ObjectFormatter"));
-
     @BuildStep
     public void registerRuntimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> resource) {
-        //org.apache.tika.parser.pdf.PDFParser (https://issues.apache.org/jira/browse/PDFBOX-4548)
-        //        resource.produce(new RuntimeInitializedClassBuildItem("org.apache.pdfbox.pdmodel.font.PDType1Font"));
-        resource.produce(new RuntimeInitializedClassBuildItem(IFUtil.class.getCanonicalName()));
         resource.produce(new RuntimeInitializedClassBuildItem(ImageImplRegistry.class.getCanonicalName()));
-        //        resource.produce(new RuntimeInitializedClassBuildItem(FopFactory.class.getCanonicalName()));
-        //        resource.produce(new RuntimeInitializedClassBuildItem("sun.font.TrueTypeFont"));
-        //        resource.produce(new RuntimeInitializedClassBuildItem("sun.font.SunFontManager"));
-        //        resource.produce(new RuntimeInitializedClassBuildItem("sun.awt.X11FontManager"));
     }
 
 }
