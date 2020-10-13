2237c8b148ea7e4351a0dca5de13905070aa191b
JiriOndrusek <ondrusek.jiri@gmail.com>
2020-10-13 15:13:58 +0200

    CassandraQL native support #1355

---
+++ extensions/cassandraql/deployment/src/main/java/org/apache/camel/quarkus/component/cassandraql/deployment/CassandraqlProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.cassandraql.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
+import org.apache.camel.quarkus.component.CamelCassandraqlConfig;
+
+class CassandraqlProcessor {
+
+    private static final String FEATURE = "camel-cassandraql";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    SystemPropertyBuildItem eagerSessionInit(CamelCassandraqlConfig config) {
+        //if property eager-session-init is null, override cassandra-quarkus with false
+        return config.eagerSessionInit.isPresent() ? null
+                : new SystemPropertyBuildItem("quarkus.cassandra.init.eager-session-init", "false");
+    }
+}
