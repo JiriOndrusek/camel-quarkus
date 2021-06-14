aa2166146372fbaf68d9a64eb217cec5660cea0b
JiriOndrusek <ondrusek.jiri@gmail.com>
2021-06-14 09:29:06 +0200

    working poc

--- integration-tests/avro-rpc/src/main/java/org/apache/camel/quarkus/component/avro/rpc/it/HttpAvroRpcServer.java
+++ integration-tests/avro-rpc/src/main/java/org/apache/camel/quarkus/component/avro/rpc/it/HttpAvroRpcServer.java
@@ -1,18 +1,14 @@
 package org.apache.camel.quarkus.component.avro.rpc.it;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import io.undertow.Handlers;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
@@ -20,47 +16,39 @@ import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.api.InstanceHandle;
-import io.undertow.servlet.handlers.ServletRequestContext;
 import io.undertow.servlet.spec.HttpServletRequestImpl;
 import io.undertow.servlet.spec.HttpServletResponseImpl;
-import io.undertow.vertx.VertxHttpExchange;
 import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.ipc.Responder;
 import org.apache.avro.ipc.ResponderServlet;
 import org.apache.avro.ipc.Server;
 
-public class HttpQuarkusServer implements Server {
+public class HttpAvroRpcServer implements Server {
 
     private Undertow server;
 
     private int port;
 
-    public HttpQuarkusServer(Responder servletAvro, int port) throws IOException, ServletException {
+    public HttpAvroRpcServer(Responder servletAvro, int port) throws IOException, ServletException {
 
         DeploymentInfo servletBuilder = Servlets.deployment()
                 .setClassLoader(Thread.currentThread().getContextClassLoader())
                 .setContextPath("/*")
-                .setDeploymentName("avro")
-                .addServletContextAttribute("avro", servletAvro)
+                .setDeploymentName("avro-rpc-http")
+                .addServletContextAttribute("avro-servlet-param", servletAvro)
                 .addServlets(
-                        Servlets.servlet("MessageServlet", MessageServlet.class)
-//                                .addInitParam("message", "Hello World")
+                        Servlets.servlet("Avro-rpc servlet", AvroRpcServlet.class)
                                 .addMapping("/*"));
 
         DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
         manager.deploy();
 
         HttpHandler servletHandler = manager.start();
-        PathHandler path = Handlers.path(Handlers.redirect("/*"))
-                .addPrefixPath("/*", servletHandler);
-
-//        HttpHandler servletHandler = manager.start();
 
         this.port = port;
         server = Undertow.builder()
                 .addHttpListener(port, "localhost")
-                .setHandler(path)
+                .setHandler(new PathHandler(servletHandler))
                 .build();
     }
 
@@ -102,23 +90,10 @@ public class HttpQuarkusServer implements Server {
 
         @Override
         public void handleRequest(final HttpServerExchange exchange) throws IOException, ServletException {
-
-//            final ServletRequestContext servletRequestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
-//            ServletRequest request = servletRequestContext.getServletRequest();
-//            ServletResponse response = servletRequestContext.getServletResponse();
-//            ServletRequest request = ((VertxHttpExchange) exchange.getDelegate()).getreqrequest
-//            InstanceHandle<? extends Servlet> servlet = null;
-
-
-            try {
-                responderServlet.service(new HttpServletRequestImpl(exchange, null), new HttpServletResponseImpl(exchange, null));
-            } finally {
-//                if (servlet != null) {
-//                    servlet.release();
-//                }
-            }
+            responderServlet.service(new HttpServletRequestImpl(exchange, null), new HttpServletResponseImpl(exchange, null));
         }
     }
 
-
 }
+
+
