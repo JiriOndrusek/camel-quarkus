88ff7e18d1f974c4470b9be12c1c07c5d85ac52d
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-10-13 09:26:04 +0200

    test01

--- integration-tests/crypto/src/main/java/org/apache/camel/quarkus/component/crypto/it/CryptoRoutes.java
+++ integration-tests/crypto/src/main/java/org/apache/camel/quarkus/component/crypto/it/CryptoRoutes.java
@@ -43,10 +43,10 @@ public class CryptoRoutes extends RouteBuilder {
 
         // Crypto component using keys from a keystore
         from("direct:sign")
-                .to("crypto:sign:basic?privateKey=#myPrivateKey&algorithm=SHA1withDSA&provider=SUN&secureRandom=#customSecureRandom");
+                .to("crypto:sign:basic?privateKey=#myPrivateKey&algorithm=SHA256withDSA&provider=BCFIPS&secureRandom=#customSecureRandom");
 
         from("direct:verify")
-                .to("crypto:verify:basic?publicKey=#myPublicKey&algorithm=SHA1withDSA&provider=SUN&secureRandom=#customSecureRandom");
+                .to("crypto:verify:basic?publicKey=#myPublicKey&algorithm=SHA256withDSA&provider=BCFIPS&secureRandom=#customSecureRandom");
 
         // Crypto data format
         CryptoDataFormat cryptoDataFormat = getCryptoDataFormat();
