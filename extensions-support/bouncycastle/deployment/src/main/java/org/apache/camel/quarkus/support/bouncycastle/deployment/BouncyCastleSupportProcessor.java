fc349cd390747dd554695bc4c9a132c076c53279
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-10-13 10:56:41 +0200

    14

--- extensions-support/bouncycastle/deployment/src/main/java/org/apache/camel/quarkus/support/bouncycastle/deployment/BouncyCastleSupportProcessor.java
+++ extensions-support/bouncycastle/deployment/src/main/java/org/apache/camel/quarkus/support/bouncycastle/deployment/BouncyCastleSupportProcessor.java
@@ -16,28 +16,20 @@
  */
 package org.apache.camel.quarkus.support.bouncycastle.deployment;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
-import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeReinitializedClassBuildItem;
-import io.quarkus.security.deployment.BouncyCastleProviderBuildItem;
-import org.apache.camel.quarkus.support.bouncycastle.BouncyCastleRecorder;
 import org.jboss.jandex.IndexView;
 
 public class BouncyCastleSupportProcessor {
 
-//    @BuildStep
-//    void produceBouncyCastleProvider(BuildProducer<BouncyCastleProviderBuildItem> bouncyCastleProvider) {
-//        bouncyCastleProvider.produce(new BouncyCastleProviderBuildItem(true));
-//    }
+    //    @BuildStep
+    //    void produceBouncyCastleProvider(BuildProducer<BouncyCastleProviderBuildItem> bouncyCastleProvider) {
+    //        bouncyCastleProvider.produce(new BouncyCastleProviderBuildItem(true));
+    //    }
 
     @BuildStep
     ReflectiveClassBuildItem registerForReflection(CombinedIndexBuildItem combinedIndex) {
@@ -64,13 +56,13 @@ public class BouncyCastleSupportProcessor {
         reinitialized.produce(new RuntimeReinitializedClassBuildItem("java.security.SecureRandom"));
     }
 
-    @BuildStep
-    @Record(ExecutionTime.STATIC_INIT)
-    public void registerBouncyCastleProvider(List<CipherTransformationBuildItem> cipherTransformations,
-            BouncyCastleRecorder recorder,
-            ShutdownContextBuildItem shutdownContextBuildItem) {
-        List<String> allCipherTransformations = cipherTransformations.stream()
-                .flatMap(c -> c.getCipherTransformations().stream()).collect(Collectors.toList());
-        recorder.registerBouncyCastleProvider(allCipherTransformations, shutdownContextBuildItem);
-    }
+    //    @BuildStep
+    //    @Record(ExecutionTime.STATIC_INIT)
+    //    public void registerBouncyCastleProvider(List<CipherTransformationBuildItem> cipherTransformations,
+    //            BouncyCastleRecorder recorder,
+    //            ShutdownContextBuildItem shutdownContextBuildItem) {
+    //        List<String> allCipherTransformations = cipherTransformations.stream()
+    //                .flatMap(c -> c.getCipherTransformations().stream()).collect(Collectors.toList());
+    //        recorder.registerBouncyCastleProvider(allCipherTransformations, shutdownContextBuildItem);
+    //    }
 }
