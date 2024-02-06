bb3da51554799dce6cbffa0eb664bf61ce8497f6
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-02-06 09:39:16 +0100

    Splunk-hex native support

---
+++ integration-tests/splunk-hec/src/test/java/org/apache/camel/quarkus/component/splunk/hec/it/SplunkHecTest.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.splunk.hec.it;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@QuarkusTestResource(SplunkHecTestResource.class)
+class SplunkHecTest {
+
+    @Test
+    public void produce() throws InterruptedException {
+
+        //sleep 2h for testing
+        //        Thread.sleep(60 * 60 * 1000);
+
+        String url = String.format("http://%s:%d",
+                getConfigValue(SplunkHecResource.PARAM_REMOTE_HOST, String.class),
+                getConfigValue(SplunkHecResource.PARAM_REMOTE_PORT, Integer.class));
+        //        //
+        //                String url = "http://localhost:32781";
+        //
+        RestAssured.given()
+                .body("Hello Sheldon")
+                .post("/splunk-hec/send")
+                .then()
+                .statusCode(200);
+        //
+        //        System.out.println("Msg sent !!!!!!!!!!!!!");
+        // use awaitility instead
+        Thread.sleep(10 * 1000);
+
+        RestAssured.given()
+                .request()
+                .formParam("search", "search index=\"testindex\"")
+                .formParam("exec_mode", "oneshot")
+                //.formParam("output_mode", "json")
+                .relaxedHTTPSValidation()
+                .auth().basic("admin", "password")
+                .post(url + "/services/search/jobs")
+                .then().statusCode(200)
+                //                .extract().asString();
+                .body(org.hamcrest.Matchers.containsString("Hello Sheldon"));
+
+        //        System.out.println(r);
+
+    }
+
+    private <T> T getConfigValue(String key, Class<T> type) {
+        return ConfigProvider.getConfig().getValue(key, type);
+    }
+
+}
