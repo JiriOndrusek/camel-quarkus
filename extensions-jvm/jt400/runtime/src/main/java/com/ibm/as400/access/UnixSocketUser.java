7bc7782d26c431c89a363f5459ccfe5bcb2519f8
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-02-13 09:48:54 +0100

    backup

---
+++ extensions-jvm/jt400/runtime/src/main/java/com/ibm/as400/access/UnixSocketUser.java
@@ -0,0 +1,10 @@
+package com.ibm.as400.access;
+
+import java.io.IOException;
+
+class UnixSocketUser {
+
+    public byte[] getUserId() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+}
