2c9a4c876801ac50766b8e015813bef43f741211
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-07-16 15:32:41 +0200

    Kudu & kerberos POC

---
+++ integration-tests/kudu/src/test/java/org/apache/camel/quarkus/component/kudu/it/NamePasswordCbHandler.java
@@ -0,0 +1,36 @@
+package org.apache.camel.quarkus.component.kudu.it;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+/**
+ * Simple {@link CallbackHandler} implementation which holds name and password and fills them into {@link NameCallback}
+ * and
+ * {@link PasswordCallback}.
+ */
+public class NamePasswordCbHandler implements CallbackHandler {
+    private transient String name;
+    private transient char[] password;
+
+    public NamePasswordCbHandler(String name, char[] password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public void handle(Callback[] callbacks) throws UnsupportedCallbackException {
+        for (Callback cb : callbacks) {
+            if (cb instanceof NameCallback) {
+                NameCallback nc = (NameCallback) cb;
+                nc.setName(name);
+            } else if (cb instanceof PasswordCallback) {
+                PasswordCallback pc = (PasswordCallback) cb;
+                pc.setPassword(password);
+            } else {
+                throw new UnsupportedCallbackException(cb);
+            }
+        }
+    }
+}
