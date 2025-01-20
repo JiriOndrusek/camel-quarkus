3ccd325e1cbf4494c1d374adcc1f1437b1c0971f
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-01-20 15:59:40 +0100

    support extension

--- extensions-support/google-pubsub/runtime/src/main/java/org/apache/camel/quarkus/support/google/cloud/graal/GaxGrpcSubstitutions.java
+++ extensions-support/google-pubsub/runtime/src/main/java/org/apache/camel/quarkus/support/google/cloud/graal/GaxGrpcSubstitutions.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.quarkus.component.google.secret.manager.graalvm;
+package org.apache.camel.quarkus.support.google.cloud.graal;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
