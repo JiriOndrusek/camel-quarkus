5fdc7eeb86ff37d942676297108826de20ae7ab4
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-03-26 16:02:14 +0100

    native fixes

--- extensions/cics/deployment/src/main/java/org/apache/camel/quarkus/component/cics/deployment/CicsProcessor.java
+++ extensions/cics/deployment/src/main/java/org/apache/camel/quarkus/component/cics/deployment/CicsProcessor.java
@@ -16,15 +16,26 @@
  */
 package org.apache.camel.quarkus.component.cics.deployment;
 
+import java.util.ListResourceBundle;
+
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import org.apache.commons.pool2.impl.DefaultEvictionPolicy;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
 import org.jboss.logging.Logger;
 
 class CicsProcessor {
 
     private static final Logger LOG = Logger.getLogger(CicsProcessor.class);
     private static final String FEATURE = "camel-cics";
+    private static final DotName LIST_RESOURCE_BUNDLE_NAME = DotName.createSimple(ListResourceBundle.class.getName());
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -36,4 +47,28 @@ class CicsProcessor {
         return new ExtensionSslNativeSupportBuildItem(FEATURE);
     }
 
+    @BuildStep
+    ReflectiveClassBuildItem registerForReflection() {
+        return ReflectiveClassBuildItem
+                .builder(DefaultEvictionPolicy.class.getName(), "com.ibm.ctg.client.ClientResourceBundle")
+                .build();
+    }
+
+    @BuildStep
+    IndexDependencyBuildItem registerDependencyForIndex() {
+        return new IndexDependencyBuildItem("com.ibm", "ctgclient");
+    }
+
+    @BuildStep
+    void resourceBundles(BuildProducer<NativeImageResourceBundleBuildItem> imageResourceBundles,
+            CombinedIndexBuildItem combinedIndex) {
+        IndexView index = combinedIndex.getIndex();
+
+        index.getAllKnownSubclasses(LIST_RESOURCE_BUNDLE_NAME).stream()
+                .filter(cl -> cl.name().toString().contains("CicsResourceBundle")
+                        || cl.name().toString().contains("ClientResourceBundle"))
+                .peek(System.out::println)
+                .map(c -> new NativeImageResourceBundleBuildItem(c.name().toString()))
+                .forEach(imageResourceBundles::produce);
+    }
 }
---
+++ extensions/cics/runtime/src/main/java/com/ibm/ctg/server/logging/Log.java
@@ -0,0 +1,21 @@
+
+package com.ibm.ctg.server.logging;
+
+import java.util.Arrays;
+
+/**
+ * Class com.ibm.ctg.server.logging.Log is referenced from com.ibm.ctg.monitoring.RequestExitMonitor. Class is not part
+ * of the ctgClient.
+ * It seems, that class belongs to the server side of the ctg.
+ * This simple implementation is replacing it fully.
+ */
+public class Log {
+
+    public static void printErrorLn(String var1, int var2, Object[] var3) {
+        io.quarkus.logging.Log.errorf("Logged error: %s, %d, %s", var1, var2, Arrays.toString(var3));
+    }
+
+    public static void printInfoLn(String var1, int var2, Object[] var3) {
+        io.quarkus.logging.Log.errorf("Logged error: %s, %d, %s", var1, var2, Arrays.toString(var3));
+    }
+}
