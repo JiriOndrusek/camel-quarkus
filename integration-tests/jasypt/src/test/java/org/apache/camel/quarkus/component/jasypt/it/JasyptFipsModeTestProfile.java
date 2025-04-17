f3550ace1efd0f33fa69ca9bf6b891f236ace59e
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-04-17 13:52:28 +0200

    works

--- integration-tests/jasypt/src/test/java/org/apache/camel/quarkus/component/jasypt/it/JasyptFipsModeTestProfile.java
+++ integration-tests/jasypt/src/test/java/org/apache/camel/quarkus/component/jasypt/it/JasyptFipsModeTestProfile.java
@@ -33,8 +33,7 @@ public class JasyptFipsModeTestProfile implements QuarkusTestProfile {
                 "timer.delay.secret", "ENC(/NsF9u8xrJh/sIre0ZQtOf6DwBaVVOcQkHe3ungkmvVfUyLXgboTgunz5Rpy+C6G)",
                 "timer.repeatCount.secret", "ENC(J1sLt6MpTuCTROefLY3MwQXcbPEDXnReFqvNdf/mBta4fs2HuO1Jkl8YbASg2oVt)",
                 "%custom-profile.greeting.expression.secret",
-                "ENC(xzuMOkyUMYG8mM4qnFXKf3HxngLky9EJTeP3vvNLp2QeLaESJL77NDD00kWBQLdrlLP2WjOjx5t8Nky1G9ez5mrr3Iiqkn7x/FGGdxXX/Wga0i289k+0ThrggjpkB5D0)",
-                "%custom-profile.greeting.secret",
-                "ENC(xzuMOkyUMYG8mM4qnFXKf3HxngLky9EJTeP3vvNLp2QeLaESJL77NDD00kWBQLdrlLP2WjOjx5t8Nky1G9ez5mrr3Iiqkn7x/FGGdxXX/Wga0i289k+0ThrggjpkB5D0)");
+                "ENC(tp3QOxMouvD3oIdTXNM0uH+BtVEMCI1ak+GBTzPZOatthRP3m+ZxAg7CF0saNTmK)",
+                "%custom-profile.greeting.secret", "tp3QOxMouvD3oIdTXNM0uH+BtVEMCI1ak+GBTzPZOatthRP3m+ZxAg7CF0saNTmK");
     }
 }
